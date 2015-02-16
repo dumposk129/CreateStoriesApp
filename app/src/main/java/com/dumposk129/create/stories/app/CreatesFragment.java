548131d572b3884d22a639fb349a8304c6b8ff91
dumposk129 <dumposk129@gmail.com>
2015-02-17 02:02:38 +0700

    add floating action button in create_fragment, edit_fragment

--- app/src/main/java/com/dumposk129/create/stories/app/CreatesFragment.java
+++ app/src/main/java/com/dumposk129/create/stories/app/CreatesFragment.java
@@ -4,21 +4,36 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class CreatesFragment extends ActionBarActivity {
 
     private Toolbar mToolbar;
+    private ImageButton imgFloatingActionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creates_fragment);
+
+        //casting
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        imgFloatingActionBtn = (ImageButton) findViewById(R.id.btnFloatingActionCreateFragment);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), mToolbar);
+
+        imgFloatingActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Selected Button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/dumposk129/create/stories/app/EditsFragment.java
+++ app/src/main/java/com/dumposk129/create/stories/app/EditsFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
 
 
 public class EditsFragment extends ActionBarActivity {
 
     private Toolbar mToolbar;
+    private ImageButton imgFloatingActionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edits_fragment);
+
+        //casting
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        imgFloatingActionBtn = (ImageButton) findViewById(R.id.btnFloatingActionEditFragment);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
