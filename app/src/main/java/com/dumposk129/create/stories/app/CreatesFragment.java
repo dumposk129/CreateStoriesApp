0ef6eeec2c8a69c7ee138ab78dbd81b95dc28738
dumposk129 <dumposk129@gmail.com>
2015-02-18 00:48:55 +0700

    change from floating action button by "ImageButton" to "ImageView" by oguzbilgener

--- app/src/main/java/com/dumposk129/create/stories/app/CreatesFragment.java
+++ app/src/main/java/com/dumposk129/create/stories/app/CreatesFragment.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 
 public class CreatesFragment extends ActionBarActivity {
 
     private Toolbar mToolbar;
-    private ImageButton imgFloatingActionBtn;
+    //private ImageButton FloatingActionBtn, FloatingCharacterMenu, FloatingBackgroundMenu, FloatingSoundMenu, FloatingSubtitleMenu;
+    private  ImageView floatingBtn, iconCharacter, iconBackground, iconSound, iconSubtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,6 @@ public class CreatesFragment extends ActionBarActivity {
 
         //casting
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
-        imgFloatingActionBtn = (ImageButton) findViewById(R.id.btnFloatingActionCreateFragment);
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -29,11 +31,36 @@ public class CreatesFragment extends ActionBarActivity {
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), mToolbar);
 
-        imgFloatingActionBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Selected Button", Toast.LENGTH_LONG).show();
-            }
-        });
+
+        //Create Floating Button
+        floatingBtn = new ImageView(this);
+        floatingBtn.setImageResource(R.drawable.ring);
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this).setContentView(floatingBtn).build();
+
+        //Create Menu Items
+        iconCharacter = new ImageView(this);
+        iconCharacter.setImageResource(R.drawable.sub_ring);
+        iconBackground = new ImageView(this);
+        iconBackground.setImageResource(R.drawable.sub_ring);
+        iconSound = new ImageView(this);
+        iconSound.setImageResource(R.drawable.sub_ring);
+        iconSubtitle = new ImageView(this);
+        iconSubtitle.setImageResource(R.drawable.sub_ring);
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+        SubActionButton buttonCharacter = itemBuilder.setContentView(iconCharacter).build();
+        SubActionButton buttonBackground = itemBuilder.setContentView(iconBackground).build();
+        SubActionButton buttonSound = itemBuilder.setContentView(iconSound).build();
+        SubActionButton buttonSubtitle = itemBuilder.setContentView(iconSubtitle).build();
+
+        //Create the menu with the items:
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(buttonCharacter)
+                .addSubActionView(buttonBackground)
+                .addSubActionView(buttonSound)
+                .addSubActionView(buttonSubtitle)
+                        // ...
+                .attachTo(actionButton)
+                .build();
     }
 }
