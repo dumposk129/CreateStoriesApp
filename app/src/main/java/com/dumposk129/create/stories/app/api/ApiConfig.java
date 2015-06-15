230a3fa9112746c41c705f67198889c207ea5263
Alexander Magmai <chanaphan.brillo@gmail.com>
2015-06-15 13:11:16 +0700

    Merge branch 'master' of /Users/ctadeesom/AndroidStudioProjects/CreateStoriesApp with conflicts.

--- app/src/main/java/com/dumposk129/create/stories/app/api/ApiConfig.java
+++ app/src/main/java/com/dumposk129/create/stories/app/api/ApiConfig.java
@@ -1,25 +1,18 @@
 package com.dumposk129.create.stories.app.api;
 
 /**
- * Created by DumpOSK129.
+ * Created by DumpOSK129 on 4/19/2015.
  */
 public class ApiConfig {
     public static final String hostname(String api_name) {
       return  "http://dump.geozigzag.com/api/"+api_name+".php";
 
-        //dump.geozigzag.com/api/
     }
-
     public static final String TAG_SUCCESS = "success";
     public static final String POST = "POST";
     public static final String GET = "GET";
     public static final String TAG_QUIZ = "quiz";
-    public static final String TAG_QUIZ_ID = "quiz_id";
-    public static final String TAG_STORY_TITLE = "title_name";
-    public static final String TAG_QUESTION_ID = "question_id";
-    public static final String TAG_ANSWER_ID = "answer_id";
-
-    public static final int _TRUE = 1;
-    public static final int _FALSE = 0;
+    public static final String TAG_QUIZID = "quiz_id";
+    public static final String TAG_STORYTITLE = "title_name";
 
 }
